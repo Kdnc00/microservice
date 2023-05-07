d3f064299ee9d3a67a0f1bcd36012a2a798ae486
Kdnc00 <77608451+Kdnc00@users.noreply.github.com>
2023-05-07 15:37:54 +0300

    filter-service

--- filter-service/src/main/java/com/kodlamaio/filterservice/entities/Filter.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/entities/Filter.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import nonapi.io.github.classgraph.json.Id;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.StringTokenizer;
+
 import java.util.UUID;
 
 @Getter
--- filter-service/src/main/java/com/kodlamaio/filterservice/repository/FilterRepository.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/repository/FilterRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.UUID;
 
-public interface FilterRepository  extends MongoRepository<Filter, UUID> {
+public interface FilterRepository extends MongoRepository<Filter, UUID> {
 }
